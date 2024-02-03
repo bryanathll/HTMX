9fa38eeef5c60b3b0365c10594b432c0ee81790e
bryanathll <bryannatanael77@gmail.com>
2024-02-04 00:57:35 +0700

    create server.js

--- hypermediaAPI/server.js
+++ hypermediaAPI/server.js
@@ -2,3 +2,24 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) =>{
+    res.send('<h2> Welcome to the Node Hypermedia API </h2>');
+})
+
+app.post('/message', async (req, res)=>{
+    res.send(`<div><h3>Hello World</h3><div>`);
+})
+
+const PORT = process.env.PORT || 1330;
+
+app.listen (PORT, () =>{
+    console.log(`App is now running on post: ${PORT}`);
+})
